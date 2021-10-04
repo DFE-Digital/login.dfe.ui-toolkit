6bf4d728d475480881413b3b2de3ac7688102223
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-10-04 14:48:30 +0100

    Removed any references to cookie banner

--- src/gds-upgrade/javascript/vendors.js
+++ src/gds-upgrade/javascript/vendors.js
@@ -2,9 +2,3 @@ var GOVUKFrontend = require('govuk-frontend');
 GOVUKFrontend.initAll();
 
 window.Cookies = require('js-cookie');
-
-//GA Utils
-window.dataLayer = window.dataLayer || [];
-window.gtag = function () {
-    dataLayer.push(arguments);
-}
