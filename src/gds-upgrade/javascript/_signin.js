80b5704aa6cd52cae68f50d5a88108706bd367f8
Shivanshu Patel <shivanshu.patel@education.gov.uk>
2021-10-20 10:05:45 +0100

    Update class for refresh link

--- src/gds-upgrade/javascript/_signin.js
+++ src/gds-upgrade/javascript/_signin.js
@@ -102,7 +102,7 @@ NSA.signin = {
         $ul.append($li);
       });
       if (showRefreshLink) {
-        var $a = $('<a />').attr('class', 'govuk-link').attr('href', 'javascript:window.location.href=window.location.href').text('Refresh this page').css('color', '#1d70b8');
+        var $a = $('<a />').attr('class', 'govuk-link-bold').attr('href', 'javascript:window.location.href=window.location.href').text('Refresh this page').css('color', '#1d70b8');
         var $li = $('<li />').append($a);
         $ul.append($li);
       }
