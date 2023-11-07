e3112a835976583dd3cf4e9439f6daa14cad54a0
Ionut Predoi <ionut.predoi@education.gov.uk>
2023-11-07 16:34:31 +0000

    NSA-7270 - updated lastLogin filter functionality

--- src/gds-upgrade/javascript/_filters.js
+++ src/gds-upgrade/javascript/_filters.js
@@ -1,6 +1,32 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
 /* eslint-disable no-undef */
+
 function toKebabCase(str) {
   return str.toLowerCase().replace(/\s+/g, '-');
+}
+
+function hideElement() {
+  $(this).hide();
+}
+
+function uncheckCheckbox() {
+  $(this).prop('checked', false);
+}
+
+function extractStartingNumberFromId(elementId) {
+  const element = $(`#${elementId}`);
+  const startingNumber = parseInt(element.prop('id').split('-').pop(), 10);
+  return startingNumber;
+}
+
+function extractFirstPartFromId(id) {
+  const parts = id.split('-');
+  if (parts.length >= 2) {
+    parts.pop();
+    return `${parts.join('-')}-`;
+  }
+  return id;
 }
 
 function updateCount(categoryName) {
@@ -34,7 +60,30 @@ function showMe(boxId, categoryName, checkedElementId) {
   updateNoFilterMessage();
 }
 
+
+function filterAndPerformAction(firstPartOfListId, listIdStartingNumber, performAction) {
+
+  const elementsToProcess = $(`[id^="${firstPartOfListId}"]`).filter(function () {
+    const id = parseInt(this.id.split('-').pop(), 10);
+    return !Number.isNaN(id) && id > listIdStartingNumber;
+  });
+
+  elementsToProcess.each(performAction);
+}
+
 function hideMe(listId, categoryName, checkboxId) {
+  if (categoryName === 'Last login') {
+    const listIdStartingNumber = extractStartingNumberFromId(listId);
+
+    const firstPartOfListId = extractFirstPartFromId(listId);
+    const firstPartOfCheckboxId = extractFirstPartFromId(checkboxId);
+
+    filterAndPerformAction(firstPartOfListId, listIdStartingNumber, hideElement);
+    filterAndPerformAction(firstPartOfCheckboxId, listIdStartingNumber, uncheckCheckbox);
+
+    const lastLoginCheckboxes = $('.last-login-checkboxes-input');
+    lastLoginCheckboxes.prop('disabled', false);
+  }
   $(`#${listId}`).hide();
   $(`#${checkboxId}`).prop('checked', false);
 
@@ -64,6 +113,43 @@ function initializeFilters() {
   });
 }
 
-$(document).ready(() => {
+
+function handleLastLoginCheckboxChange(checkboxes, lastLoginListTags) {
+
+  checkboxes.on('change', function () {
+    const index = checkboxes.index(this);
+    let firstPartOfCheckboxId;
+    if (this.checked) {
+      switch (this.value) {
+        case 'never':
+
+          lastLoginListTags.css('display', 'none');
+          checkboxes.slice(0, index).prop('checked', false);
+          checkboxes.slice(0, index).prop('disabled', true);
+          initializeFilters();
+          break;
+
+        default:
+          checkboxes.slice(0, index).prop('checked', true);
+          initializeFilters();
+      }
+    } else {
+      firstPartOfCheckboxId = extractFirstPartFromId(this.id);
+      filterAndPerformAction(firstPartOfCheckboxId, index, uncheckCheckbox);
+      checkboxes.slice(0, index).prop('disabled', false);
+      filterAndPerformAction('last-login-option-', index, hideElement);
+      initializeFilters();
+    }
+  });
+}
+
+$(() => {
   initializeFilters();
+
+  const lastLoginCheckboxes = $('.last-login-checkboxes-input');
+  const lastLoginListTags = $('.last-login-option');
+
+  lastLoginCheckboxes.on('click', () => {
+    handleLastLoginCheckboxChange(lastLoginCheckboxes, lastLoginListTags);
+  });
 });
